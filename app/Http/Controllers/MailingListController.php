60ea37ac9f662ebaee5048727be0f6e310b97f15
Ernie Dipko <ernie.dipko@gmail.com>
2024-12-01 14:50:28 -0500

    Add mailing list controller

--- app/Http/Controllers/MailingListController.php
+++ app/Http/Controllers/MailingListController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\MailingList;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class MailingListController extends Controller
 {
@@ -23,17 +24,60 @@ class MailingListController extends Controller
 
     public function sendEmails(Request $request)
     {
+        $request->validate([
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string',
+        ]);
+
         $users = MailingList::all();
         $subject = $request->subject;
         $messageBody = $request->message;
 
         foreach ($users as $user) {
-            Mail::raw($messageBody, function ($message) use ($user, $subject) {
-                $message->to($user->email);
-                $message->subject($subject);
-            });
+            $this->sendEmailUsingSendGrid($user->email, $subject, $messageBody);
         }
 
         return redirect()->route('mailing-list.index')->with('success', 'Emails sent successfully.');
+    }
+
+    private function sendEmailUsingSendGrid($email, $subject, $messageBody)
+    {
+        $sendGridApiKey = env('SENDGRID_API_KEY');
+        $sendGridUrl = 'https://api.sendgrid.com/v3/mail/send';
+
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $sendGridApiKey,
+            'Content-Type' => 'application/json',
+        ])->post($sendGridUrl, [
+            'personalizations' => [
+                [
+                    'to' => [
+                        ['email' => $email],
+                    ],
+                    'subject' => $subject,
+                ],
+            ],
+            'from' => [
+                'email' => 'website@boomtownpa.com', // Replace with your verified "from" email
+                'name' => 'Boomtown Mailing List',
+            ],
+            'content' => [
+                [
+                    'type' => 'text/plain',
+                    'value' => $messageBody,
+                ],
+            ],
+        ]);
+
+        if (!$response->successful()) {
+            Log::error('Failed to send email via SendGrid', [
+                'email' => $email,
+                'response' => $response->body(),
+            ]);
+        } else {
+            Log::info('Email sent successfully via SendGrid', [
+                'email' => $email,
+            ]);
+        }
     }
 }
