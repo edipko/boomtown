409888ae6c8f963eef03bcff6c33bfb115962f57
Ernie Dipko <ernie.dipko@gmail.com>
2024-12-01 15:08:31 -0500

    Add mailing list unsubscribe

--- database/migrations/2024_12_01_200410_add_unsubscribe_token_to_mailing_list.php
+++ database/migrations/2024_12_01_200410_add_unsubscribe_token_to_mailing_list.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('mailing_list', function (Blueprint $table) {
+        Schema::table('mailing_lists', function (Blueprint $table) {
             $table->string('unsubscribe_token')->unique()->nullable();
         });
     }
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('mailing_list', function (Blueprint $table) {
+        Schema::table('mailing_lists', function (Blueprint $table) {
             $table->dropColumn('unsubscribe_token');
         });
     }
