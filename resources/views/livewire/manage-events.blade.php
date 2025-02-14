d3ca7b94e9787ec04b3c9aabda204ab7bc970a00
Ernie Dipko <ernie.dipko@gmail.com>
2025-02-14 10:48:11 -0500

    Fix EventList management

--- resources/views/livewire/manage-events.blade.php
+++ resources/views/livewire/manage-events.blade.php
@@ -11,7 +11,7 @@
         </tr>
         </thead>
         <tbody>
-        @foreach($upcomingEvents as $event)
+        @forelse($upcomingEvents as $event)
             <tr class="border">
                 <td class="border p-2">{{ $event->name }}</td>
                 <td class="border p-2">{{ $event->date }}</td>
@@ -22,7 +22,11 @@
                     <button wire:click="deleteEvent({{ $event->id }})" class="text-red-600">Delete</button>
                 </td>
             </tr>
-        @endforeach
+        @empty
+            <tr>
+                <td colspan="5" class="text-center p-2">No upcoming events.</td>
+            </tr>
+        @endforelse
         </tbody>
     </table>
 
@@ -34,4 +38,26 @@
             <th class="border p-2">Date</th>
             <th class="border p-2">Time</th>
             <th class="border p-2">Venue</th>
-            <th class="border
+            <th class="border p-2">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @forelse($pastEvents as $event)
+            <tr class="border text-gray-500">
+                <td class="border p-2">{{ $event->name }}</td>
+                <td class="border p-2">{{ $event->date }}</td>
+                <td class="border p-2">{{ $event->time }}</td>
+                <td class="border p-2">{{ $event->venue->name }}</td>
+                <td class="border p-2">
+                    <a href="{{ route('events.edit', $event->id) }}" class="text-blue-600">Edit</a> |
+                    <button wire:click="deleteEvent({{ $event->id }})" class="text-red-600">Delete</button>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="5" class="text-center p-2">No past events.</td>
+            </tr>
+        @endforelse
+        </tbody>
+    </table>
+</div>
